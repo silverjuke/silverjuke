aad86d1cc7fc3eab1c5c27b21ffbf632063b5eb5
B. Petersen <r10s@b44t.com>
2015-11-12 15:44:37 +0100

    Character '&' must not be escabed for values in wxTextCtrl (we used wxStaticText here before)

--- src/sjmodules/tageditor/tageditorrename.cpp
+++ src/sjmodules/tageditor/tageditorrename.cpp
@@ -257,11 +257,6 @@ void SjRenamePlugin::UpdateExample()
 	{
 		wxString pathPartNFileName = m_patternCtrl->GetValue();
 		m_tiReplacer.ReplacePath(pathPartNFileName, m_exampleTrackInfo);
-
-		#ifdef __WXMSW__
-			pathPartNFileName.Replace(wxT("&"), wxT("&&")); // "&" is converted to an underscore for the next character on MSW
-		#endif
-
 		m_exampleCtrl->SetValue(pathPartNFileName);
 	}
 }
