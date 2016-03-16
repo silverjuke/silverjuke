fb8ffd8b3415e1d135392b38a5ca58c3e42367bf
B. Petersen <r10s@b44t.com>
2016-03-16 13:16:48 +0100

    No abort() while parsing presets.

--- src/prjm/src/MilkdropPresetFactory/Parser.cpp
+++ src/prjm/src/MilkdropPresetFactory/Parser.cpp
@@ -199,7 +199,7 @@ sj_token_t Parser::parseToken(std::istream &  fs, char * string)
 			if (fs)
 				fs.unget();
 			else
-				abort();
+				return tEOL;//abort(); - EDIT BY SJ - everything, but not program abort!
 		}
 		return tEOL;
 	}
@@ -1735,7 +1735,7 @@ void Parser::readStringUntil(std::istream & fs, std::string * out_buffer, bool w
 							if (fs)
 								fs.unget();
 							else
-								abort();
+								return; // abort(); -- EDIT BY SJ -- everything but no program abort!
 						}
 						return;
 					}
